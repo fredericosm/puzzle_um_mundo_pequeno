e06a88584d2d6a4e162119b324fa12b31f13d520
Germano Nicolini <germanogmn1@gmail.com>
2012-02-19 15:54:48 -0200

    friend validations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,10 @@
+# encoding: utf-8
+
 class Friend < ActiveRecord::Base
+  validates :name, presence: true
+  validates :latitude, presence: true,
+                       uniqueness: {
+                          scope: :longitude,
+                          message: "Essa localização já está sendo usada"}
+  validates :longitude, presence: true
 end
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Friend do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "requires name presence" do
+    subject.should have_at_least(1).error_on(:name)
+    subject.name = "Fulano"
+    subject.should have(:no).error_on(:name)
+  end
+  
+  it "requires latitude/longitude presence" do
+    subject.should have_at_least(1).error_on(:latitude)
+    subject.should have_at_least(1).error_on(:longitude)
+  end
+  
+  it "requires a unique latitude/longitude" do
+    described_class.create name: 'Joao', latitude: '35', longitude: '51'
+    subject.name = 'Foo' # giving a name for the record to be valid
+    subject.latitude = '35'
+    subject.longitude = '51'
+    subject.should_not be_valid
+    subject.longitude = '52'
+    subject.should be_valid
+  end
 end
