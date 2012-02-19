c657d2608743891b4721ac2bba27f7f812fe46a5
Germano Nicolini <germanogmn1@gmail.com>
2012-02-19 14:42:45 -0200

    rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuzzleUmMundoPequeno::Application.config.secret_token = '4154ae5bf6fb122fc9ef803721e0547aa88c8062ea3346f63afee9a7a469ee16cdfcf336f0f15993e5b5777bfb3f5f48f48f220126071a5e688ff15fe0aa255c'
